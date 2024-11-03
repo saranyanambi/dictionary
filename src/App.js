fdedb0a140566e7fbd47c9bd87b8bb1518b882d0
saranya <saranyanambi911@gmail.com>
2024-11-03 08:17:05 +0530

    dict implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const arr=
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ]
+
+  
+  
+  const [query,setQuery]=useState("")
+  const [filteredword,setFilteredword]=useState("");
+  const [searched,setSearched]=useState(false);
+  const handleSearch=()=>{
+    setSearched(true)
+      const word=arr.find(item=>query.toLowerCase()==item.word.toLowerCase())
+      if(word)
+        setFilteredword(word.meaning);
+      else
+        setFilteredword("")
+      setQuery("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+     <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)} placeholder='Search for word...'/>
+     <button onClick={handleSearch}>Search</button>
+     <h6>Definition:</h6>
+
+     {searched && filteredword ? (
+        <p>{filteredword}</p>
+      ) : (
+        searched && <p>Word not found in the dictionary.</p>
+      )}
     </div>
   );
 }
